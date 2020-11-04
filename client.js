3967db3a5b6c4cfdd487543a7fd7099e4dd648f6
Kevinli296 <kevinli296@outlook.com>
2020-11-04 21:55:18 +0000

    added names to users on connection

--- client.js
+++ client.js
@@ -11,6 +11,10 @@ const connect = function() {
   });
     // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    conn.write('Name: KL');
+  });
   
   conn.on('data', (data) => {
     console.log(data);
